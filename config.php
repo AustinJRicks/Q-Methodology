4bac3de60669ee903531fa19a67ba5f5f4a31fba
AustinJRicks <Ricks@planetmail.net>
2016-07-01 08:20:54 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+########## MySql details (Replace with yours) #############
+$username = "9bbcef_test"; //mysql username
+$password = "Maxkornev1"; //mysql password
+$hostname = "MYSQL5003.Smarterasp.net"; //hostname
+$databasename = "db_9bbcef_test"; //databasename
+
+//connect to database
+$mysqli = new mysqli($hostname, $username, $password, $databasename);
+
+
+?>
+
